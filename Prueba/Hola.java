f76338a9621f848d21a944a96eb6b623f7d9bfaf
Kevin Miranda <aa22.kmiranda24@iespabloserrano.com>
2025-09-25 08:55:49 +0200

    Agregación de saludo personalizado
    Se modifico el archivo de hola.java para mostrar un saludo dependiendo de la persona que lo ejecute

---
+++ Prueba/Hola.java
@@ -0,0 +1,15 @@
+public class Hola {
+
+    public static void main(String[] args) {
+        
+        String nombre;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime tu nombre:");
+        nombre = sc.nextLine();
+
+        System.out.println("Salu2 " + nombre);
+
+
+    }
+}
